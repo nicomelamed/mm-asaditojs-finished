8c5505ffcb5ef49f101cb61528964d4e9abbf948
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-01-09 10:48:39 -0300

    remove implicit returns

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./title.css";
 
-const Title = () => (
-  <>
-    <h1 className='title'>pick your favorite gif cat</h1>
-  </>
-);
+const Title = () => {
+  return (
+    <>
+      <h1 className="title">pick your favorite gif cat</h1>
+    </>
+  );
+};
 
 export default Title;
