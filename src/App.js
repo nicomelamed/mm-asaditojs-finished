7a3aabe67a5511d23727372285d99177b19eda34
nmelamed <nmelamed@glamit.com.ar>
2020-01-22 23:27:20 -0300

    add catch error to fetch api call

--- src/App.js
+++ src/App.js
@@ -14,16 +14,17 @@ const App = () => {
     const response = await data.json();
     setGifUrl(setGifUrl(response.data.fixed_height_downsampled_url));
 
-    /* 
+    /*
     Fetch API using Promises
-    
+
     fetch(
       "https://api.giphy.com/v1/gifs/random?api_key=gH7glaXpjdIJUfGdtE2GO5FSQzi09bbY&tag=Cat&rating=G"
     )
       .then(data => data.json())
       .then(response => {
         setGifUrl(response.data.fixed_height_downsampled_url);
-      });
+      })
+      .catch(() => console.log("Hubo un error en la llamada a la api");
     */
   };
 
